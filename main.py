1c4b9bde825fdcf439a6b1f1a784799b69fd45d8
Nikhil <n.s_10@live.com>
2024-09-24 16:25:14 -0400

    update

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import os
+from dotenv import load_dotenv
+
 import langchain
-import langsmith
-import langserve
+from langchain_openai import ChatOpenAI
+
+# import langsmith
+# import langserve
+
+load_dotenv()
+
+input_phrase = 'Cest quoi la fromage?'
+
+model = ChatOpenAI(model='gpt-4o-mini', temperature=0)
+
